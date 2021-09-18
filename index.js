4bcf475608634996e693b8be7db97fa7ed12d54e
Akhil Vanka <58049327+akhilvanka@users.noreply.github.com>
2021-09-18 12:04:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(session({
   secret: process.env.SECRET 
 })); // Set up the app using sessions
 
-app.use(__dirname + express.static('/views'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
   res.render('pages/auth');
